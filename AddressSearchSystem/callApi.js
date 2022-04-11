2b0dc54d991f77a8123d78263c3344bef08a126b
mikiyuta <popporio9@outlook.jp>
2022-04-11 21:12:33 +0900

    コメントの追加

--- AddressSearchSystem/callApi.js
+++ AddressSearchSystem/callApi.js
@@ -1,4 +1,4 @@
-
+//受け取ったAPIデータをWEB上に表示する処理
 function getAddressData(addressData) {
     const combinedAddress = `${addressData.address1}${addressData.address2}${addressData.address3}`;
     const newP = document.createElement("p");
@@ -6,6 +6,7 @@ function getAddressData(addressData) {
     output.appendChild(newP);
 }
 
+//入力した郵便番号を元にAPIデータを受け取る処理
 async function callApi() {
     const zipcode = document.getElementById("zipcode").value;
     const testResult = zipcode.match(/\d{3}-?\d{4}/);
